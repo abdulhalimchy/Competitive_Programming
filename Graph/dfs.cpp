9db057d06a708aa4deb832cbfdb5eb871d7ddf5a
Abdul Halim Chowdhury <abdulhalim.programmer@gmail.com>
2019-06-23 02:26:57 +0600

    Create dfs.cpp

---
+++ Graph/dfs.cpp
@@ -0,0 +1,10 @@
+void dfs(int src)
+{
+    visit[src]=true;
+    vector<int> ::iterator it;
+    for(it=v[src].begin(); it!=v[src].end(); it++)
+    {
+        if(visit[*it]==false)
+            dfs(*it);
+    }
+}
