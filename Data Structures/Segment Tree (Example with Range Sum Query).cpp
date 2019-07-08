f5299361cc4f4beb4c83df36486294b2d7093821
Abdul Halim Chowdhury <abdulhalim.programmer@gmail.com>
2019-07-08 19:00:12 +0600

    Create Segment Tree (Example with Range Sum Query).cpp

---
+++ Data Structures/Segment Tree (Example with Range Sum Query).cpp
@@ -0,0 +1,75 @@
+/*:::::::::::::Segment Tree example with Range Sum Query::::::::::::::::
+Build Complexity: O(nlogn), Update Complexity: O(logn), Query Complexity: O(logn)
+*/
+
+#include<bits/stdc++.h>
+#define MX 100005
+#define ll long long
+using namespace std;
+ll sum[3*MX];
+int ara[MX];
+
+void build(int node, int L, int R)
+{
+    if(L==R)
+    {
+        sum[node]=ara[L];
+        return;
+    }
+    int mid=(L+R)/2;
+    build(node*2, L, mid);
+    build(node*2+1, mid+1, R);
+    sum[node]=sum[node*2]+sum[node*2+1];
+}
+
+ll query(int node, int L, int R, int l, int r)
+{
+    if(r<L || R<l)
+        return 0;
+    if(L>=l && R<=r)
+        return sum[node];
+    int mid=(L+R)/2;
+    ll x=query(node*2, L, mid, l, r);
+    ll y=query(node*2+1, mid+1, R, l, r);
+    return x+y;
+
+}
+
+void update(int node, int L, int R, int updateAt, int value)
+{
+    if(updateAt<L || updateAt>R)
+        return;
+    if(L==R)
+    {
+        sum[node]=value;
+        return;
+    }
+
+    int mid=(L+R)/2;
+    if(updateAt<=mid)
+        update(node*2, L, mid, updateAt, value);
+    else
+        update(node*2+1, mid+1, R, updateAt, value);
+    sum[node]=sum[node*2]+sum[node*2+1];
+}
+
+int main()
+{
+    int t;
+    scanf("%d", &t);
+    for(int tt=1; tt<=t; tt++)
+    {
+        int n;
+        scanf("%d", &n);
+        for(int i=1; i<=n; i++)
+            scanf("%d", &ara[i]);
+        build(1, 1, n);
+        for(int i=1; i<=q; i++)
+        {
+            int l, r;
+            scanf("%d %d", &l, &r);
+            printf("%lld\n", query(1, 1, n, l, r));
+        }
+    }
+    return 0;
+}
