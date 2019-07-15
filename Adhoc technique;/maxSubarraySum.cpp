5755ef9de02ee5148f74021dde4ae092661efde7
Abdul Halim Chowdhury <abdulhalim.programmer@gmail.com>
2019-07-15 18:14:33 +0600

    Update maxSubarraySum.cpp

--- Adhoc technique;/maxSubarraySum.cpp
+++ Adhoc technique;/maxSubarraySum.cpp
@@ -2,45 +2,44 @@
 /*Given an array with n positive and negative numbers, find the subarray with one or more
 consecutive numbers where the sum of the subarray is maximum.
 
-Tutorial link: https://www.geeksforgeeks.org/largest-sum-contiguous-subarray/
-*/
-#include<iostream>
-#include<climits>
+#include<bits/stdc++.h>
+#define MX 1000
 using namespace std;
-
-int maxSubArraySum(int a[], int size)
-{
-    int max_so_far = INT_MIN, max_ending_here = 0,
-       start =0, end = 0, s=0;
-
-    for (int i=0; i< size; i++ )
-    {
-        max_ending_here += a[i];
-
-        if (max_so_far < max_ending_here)
-        {
-            max_so_far = max_ending_here;
-            start = s;
-            end = i;
-        }
-
-        if (max_ending_here < 0)
-        {
-            max_ending_here = 0;
-            s = i + 1;
-        }
-    }
-    cout << "Maximum contiguous sum is "
-        << max_so_far << endl;
-    cout << "Starting index "<< start
-        << endl << "Ending index "<< end << endl;
-    return max_so_far;
-}
 
 int main()
 {
-    int a[] = {-2, -3, -1, -4, -6};
-    int n = sizeof(a)/sizeof(a[0]);
-    int max_sum = maxSubArraySum(a, n);
-    return 0;
+    int n, ara[MX];
+    scanf("%d", &n);
+    int mx=-INT_MIN;
+    int sum=0, l=1, r=1, temp=1;
+    for(int i=1; i<=n; i++)
+    {
+        scanf("%d", &ara[i]);
+        sum+=ara[i];
+        if(mx<sum)
+        {
+            mx=sum;
+            l=temp;
+            r=i;
+        }
+        if(sum<0)
+        {
+            temp=i+1;
+            sum=0;
+        }
+    }
+
+    for(int i=l; i<=r; i++)
+        cout << ara[i] << " ";
+    cout << endl;
+
+    cout << mx << endl;
 }
+/*
+9
+-2 1 -3 4 -1 2 1 -5 4
+
+7
+5 -10 6 -2 4  -10 1
+*/
+
