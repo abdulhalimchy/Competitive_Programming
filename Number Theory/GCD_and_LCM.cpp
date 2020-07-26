81fdde93d7e96332208bd22578e81f060f0a97d6
Abdul Halim Chowdhury <abdulhalim.programmer@gmail.com>
2020-07-27 05:04:37 +0600

    added function for finding lcm

--- Number Theory/GCD_and_LCM.cpp
+++ Number Theory/GCD_and_LCM.cpp
@@ -1,4 +1,4 @@
-/*::::::::::::::::::::::GCD USING RECURSION:::::::::::::::::::::::*/
+/*::::::::::::::::::::::GCD and LCM:::::::::::::::::::::::*/
 #include<iostream>
 
 //Applying Euclidean algorithm technique to find gcd
@@ -8,8 +8,13 @@ int gcd(int a, int b){
     return gcd(b%a,a);
 }
 
+int lcm(int a, int b){
+    return (a*b)/gcd(a,b);
+}
+
 int main(){
     int a, b;
     std::cin >> a >> b;
     std::cout << gcd(a,b) << std::endl;
+    std::cout << lcm(a,b) << std::endl;
 }
